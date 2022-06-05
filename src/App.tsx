2361e0b0dd2c4d18ac8928b7f0e33b08ed9cca3d
kouml <key.mtrgtring@gmail.com>
2022-06-06 02:03:08 +0900

    set timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 export function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(
+    function () {
+      const intervalId = setInterval(function () {
+        setCounter(counter + 0.1);
+      }, 100);
+      return function () {
+        clearInterval(intervalId);
+      };
+    },
+    [counter]
+  );
+
   return (
     <div className="App">
-      <h1>Menu bar</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Vocab App</h1>
+      <h2>Seconds: {counter}</h2>;
     </div>
   );
 }
